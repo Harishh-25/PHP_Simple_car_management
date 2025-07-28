9ddd519917c890f496e9a0b95a919a5cb7f5925d
Harishh-25 <yttharish@gmail.com>
2025-07-28 13:37:32 +0530

    Create option.php

---
+++ option.php
@@ -0,0 +1,14 @@
+form input, form select, form button {
+margin: 5px 0;
+padding: 8px;
+width: 100%;
+}
+table {
+margin-top: 10px;
+width: 100%;
+border-collapse: collapse;
+}
+th, td {
+padding: 10px;
+border: 1px solid #ccc;
+}
