26b8f98cc48e2033d8dfd98569df529830a34bf1
Harishh-25 <yttharish@gmail.com>
2025-07-28 13:31:24 +0530

    Create db.php

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "car
+service
+db";
+_
+_
+$conn = new mysqli($host, $username, $password, $database);
+if ($conn->connect
+_
+error) {
+die("Connection failed: "
+. $conn->connect
+_
+error);
+}
+?>
